a577d37e8c972f3bbf2cc95303b6d883cca2e5bf
50316004 <f0316004@ipc.shizuoka.ac.jp>
2015-02-12 10:44:29 +0900

    first commit

--- src/dbconsole/DBConsole_main.java
+++ src/dbconsole/DBConsole_main.java
@@ -11,30 +11,35 @@ public class DBConsole_main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Label total1, year1, heisei1;
+		Label total1, year1, heisei1, nin1;
 		int total, year;
-		String heisei;
+		String heisei, nin;
 		ResultSet rs;
 		MySQL mysql = new MySQL();
 		rs = mysql.selectAll();
 		FrameSystemController controller = new FrameSystemController();
 		FrameSystemView frame = new FrameSystemView(controller);
-		frame.setBounds(10,10,655,455);
+		frame.setBounds(10,10,255,955);
 		frame.setVisible(true);
 try {
 	while(rs.next()){	
 		total = rs.getInt("total");
 		heisei = rs.getString("heisei");	
 		year = rs.getInt("year");
-	    System.out.println("total：" + total);
+		nin = rs.getString("nin");	
 	    System.out.println("heisei：" + heisei);
 	    System.out.println("year：" + year); 
+	    System.out.println("total：" + total);
+	    System.out.println("nin：" + nin); 
+		System.out.println("\n");
 	    total1 = new Label(String.valueOf(total));
 	    year1 = new Label(String.valueOf(year));
 	    heisei1 = new Label(String.valueOf(heisei));
+	    nin1 = new Label(String.valueOf(nin));	    
 	    frame.add(heisei1);                
 	    frame.add(year1);
 	    frame.add(total1);
+	    frame.add(nin1);	    
 	  }
 } catch (SQLException e) {
 	// TODO Auto-generated catch block
--- src/lib1/FrameSystemView.java
+++ src/lib1/FrameSystemView.java
@@ -3,6 +3,7 @@ package lib1;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Frame;
+import java.awt.GridLayout;
 import java.awt.Label;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
@@ -12,31 +13,21 @@ import java.awt.event.WindowListener;
 
 //import org.jfree.chart.ChartPanel;
 
+
 import dbconsole.MySQL;
 public class FrameSystemView extends Frame implements ActionListener, WindowListener {
 	public FrameSystemView(FrameSystemController controller) {
 		// TODO Auto-generated constructor stub
-		String total, year;
 		addWindowListener(this);
-		setTitle("FrameSystem");
-		setLayout(new FlowLayout(FlowLayout.CENTER));
+		setTitle("Japanese Total Population");
+		setLayout(new GridLayout(8,4));
 		MySQL clib = new MySQL();		
-//		add(text1);
-//		add(text2);
-//	      clib.setText(String.valueOf(total));
-//	      text2.setText(String.valueOf(2));
-//	    ChartPanel cpanel = new ChartPanel(chart);
-//		    add(cpanel, BorderLayout.CENTER);
-
 	}
 
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-//		if(e.getSource() == button1){			
-//		int val1 = Integer.parseInt(text1.getText());
-//		int val2 = Integer.parseInt(text2.getText());
-
 	}
 
 	@Override
